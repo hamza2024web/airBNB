e99c0cf98209d5f61e072a25f8f0587904674b15
hamza2024web <hamzaakroubi1528@gmail.com>
2025-02-07 17:01:01 +0100

    MVC

---
+++ project/Config/Database.php
@@ -0,0 +1,46 @@
+<?php
+namespace Config;
+
+require_once("../vendor/autoload.php");
+use Dotenv\Dotenv;
+use PDO;
+use PDOException;
+
+class Database
+{
+
+    private static $pdoSinglton;
+
+    public static function getConnection()
+    {
+
+
+        if (self::$pdoSinglton != null) {
+            return self::$pdoSinglton;
+        } else {
+
+
+            try {
+
+                $dotenv = Dotenv::createImmutable(__DIR__.'/../');
+                $dotenv->load();
+
+
+                $dsn = "pgsql:host=".$_ENV['DB_HOST'].";dbname=".$_ENV['DB_NAME'];
+                $pdo_instance = new PDO($dsn, $_ENV['DB_USER'],$_ENV['DB_PASSWORD']);
+
+                self::$pdoSinglton =  $pdo_instance ;
+                echo "sf connexion dazt";
+                return self::$pdoSinglton;
+                
+            } catch (PDOException $e) {
+                print_r($_ENV);
+                echo "Connection error:".$e->getMessage();
+            }
+
+
+        }
+
+
+    }
+}
---
+++ project/src/Http/Request.php
@@ -0,0 +1,23 @@
+<?php 
+namespace Src\Http;
+
+class Request {
+
+    public function Methode() {
+        return strtolower($_SERVER['REQUEST_METHOD']);
+    }
+
+    public function path (){
+        $path = $_SERVER["REQUEST_URI"];
+
+        $path = str_replace( '/public/', '',$path);
+
+        $path = trim($path, '/');
+
+        if (str_contains($path, '?')) {
+            return explode('?', $path)[0];
+        }
+        
+        return $path;
+    }
+}
